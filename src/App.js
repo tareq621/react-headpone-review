14c8ae62f45e00d60de178b51135710dcf9fae8f
tareq621 <mirtareq621@gmail.com>
2022-04-05 21:22:07 +0600

    about page refresh route set and complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Component/About/About';
 import Blogs from './Component/Blogs/Blogs';
 import Dashboard from './Component/Dashboard/Dashboard';
 import Header from './Component/Header/Header';
@@ -17,6 +18,7 @@ function App() {
         <Route path='/reviews' element={<Reviews></Reviews>}></Route>
         <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
